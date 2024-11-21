2eb649cdc32332e5079a8434ebea2eefbd3a4adf
Gramaso <gutierrez.caceres.antonio@iescamas.es>
2024-11-21 14:13:53 +0100

    Config

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentos42024";
+     static public $mvc_bd_usuario  = "mypanchonb0";
+     static public $mvc_bd_clave    = "22bN2m3e";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
